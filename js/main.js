0b266d2d0ad48fa45782d1e7623267aa55d079e6
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-02-04 15:33:19 -0300

    navbar

--- js/main.js
+++ js/main.js
@@ -13,13 +13,20 @@ function toggleMenu() {
         alter_content_by("section","width","90vw");
         alter_content_by("section","fontSize","1.2em");
         alter_content_by(".toggleMenu","opacity","0.7");
-        toggleStatus = 0;
+        alter_content_by(".Questao2","fontSize","1em");
+        alter_content_by(".Questao3","fontSize","1em");
+        alter_content_by(".Questao4","fontSize","0.9em");
+        toggleStatus = 0
     }
     else if (toggleStatus == 0) {
         document.getElementById("menu").style.left = "0";
         alter_content_by("section","left","30vw");
         alter_content_by("section","width","65vw");
         alter_content_by("section","fontSize","1em");
+        alter_content_by(".Questao2","fontSize","0.8em");
+        alter_content_by(".Questao3","fontSize","0.8em");
+        alter_content_by(".Questao4","fontSize","0.8em");
+
         toggleStatus = 1;
     }
 }
@@ -49,9 +56,6 @@ function init() {
     scroll_function();
     alter_content_by("section","left","30vw");
     alter_content_by("section","width","65vw");
-
-
-
 }
 // inicializando
 init();
